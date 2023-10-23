024ae27e4c2f197580e0584f3f1955545b9444cc
Jezebel <me@jezebel.dev>
2023-10-23 09:23:41 -0400

    update

--- src/jsx/discord.js
+++ src/jsx/discord.js
@@ -15,9 +15,12 @@ const Discord = () => {
   };
 
   return (<>
-    <div style={{cursor: 'pointer', display: 'flex',
+    <div style={{
+      cursor: 'pointer', display: 'flex',
       flexDirection: 'row',
+      flexWrap: 'wrap',
       alignItems: 'center',
+      fontSize: '1rem',
       marginBlock: '1em'}}
        onClick={discordInvite}>
       <img src={logo} width={'100px'} height={'100px'}
@@ -31,9 +34,11 @@ const Discord = () => {
       <div style={{color: '#6BA6B2'}}>
         United
       </div>
+      <div style={{display: 'flex', flex: 'none'}}>
       <img src={discord} width={'50px'} height={'50px'}
            alt={'Discord Invite'} style={{marginLeft: '10px'}}/>
       <p style={{marginLeft: '10px'}}>invite.</p>
+      </div>
     </div>
   </>);
 };
